3f08e1bb87e397155c5b9ba5cf76a5e18917ac9f
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-03-23 23:08:44 +0300

    Added css btn and rearranged code using prettier

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,26 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import spaceVideo from "../assests/space.mp4"
+import spaceVideo from "../assests/space.mp4";
 import "./Video.css";
 
 function Video() {
   return (
     <div className="hero">
-      <video src={spaceVideo}  autoPlay muted loop id="video">
+      <video src={spaceVideo} autoPlay muted loop id="video">
         {/* <source src={spaceVideo} type="video.mp4" /> */}
       </video>
       <div className="content">
         <h1>Galaxy. Travel.</h1>
-        <p>Explore the wonders of the universe with Galaxy Travel - your ticket to a journey beyond your wildest dreams.</p>
-      
+        <p>
+          Explore the wonders of the universe with Galaxy Travel - your ticket
+          to a journey beyond your wildest dreams.
+        </p>
 
-      <div>
-      <Link to='./training' className= 'btn' >Training</Link>
-      <Link to='./contact' className='btn btn-light'>Launch</Link>
+        <div>
+          <Link to="./training" className="btn">
+            Training
+          </Link>
+          <Link to="./contact" className="btn btn-light">
+            Launch
+          </Link>
+        </div>
       </div>
-
-
-    </div>
     </div>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Video from '../components/Video';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Video from "../components/Video";
 
 function Home() {
   return (
     <div>
-        <Navbar />
-        <Video />
+      <Navbar />
+      <Video />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

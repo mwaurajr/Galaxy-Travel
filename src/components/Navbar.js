4be6391d02fbfaad8263e0f48a77aa01ee54f2ec
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-03-23 12:44:16 +0300

    Stled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Navbar.css'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 function Navbar() {
   return (
     <div  className='header'>
-        <h1>GLX-TVL</h1>
+        <Link to='/'><h1>GLX-TVL</h1></Link> 
+        <ul className='nav-menu'>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+
+            <li>
+                <Link to='/pricing'>Pricing</Link>
+            </li>
+
+            <li>
+                <Link to='/training'>Training</Link>
+            </li>
+
+            <li>
+                <Link to='/contact'>Contact</Link>
+            </li>
+
+        </ul>
+
+        <div className='hamburger'> 
+       
+        <FaBars size={20} style={{color: '#fff'}} />
+       
+        </div>
+        
+
         </div>
   )
 }

bfa77ee1fc2580f6a9db421a161fbb182f127629
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-03-26 13:09:07 +0300

    Created pricing page

---
+++ src/routes/Pricing.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+function Pricing() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default Pricing;

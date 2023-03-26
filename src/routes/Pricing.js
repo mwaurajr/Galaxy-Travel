36502db428d21a3bab908e7e1e6f2746941ba572
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-03-26 14:08:49 +0300

    Added pricing card css and js

--- src/routes/Pricing.js
+++ src/routes/Pricing.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "../components/Footer";
 import HeroImage from "../components/HeroImage";
 import Navbar from "../components/Navbar";
+import PricingCard from "../components/PricingCard";
 
 function Pricing() {
   return (
     <div>
       <Navbar />
       <HeroImage  heading='PRICING.' text='Choose your Trip'/>
+      <PricingCard />
       <Footer />
     </div>
   );

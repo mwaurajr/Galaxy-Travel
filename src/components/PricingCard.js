eec242d4b9b9231f81ef0ca2368867df28f688fb
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-03-27 06:28:19 +0300

    Done with the pricing page

--- src/components/PricingCard.js
+++ src/components/PricingCard.js
@@ -7,37 +7,37 @@ function PricingCard() {
     <div className='pricing'>
       <div className="card-container">
         <div className="card">
-          <h3>- Basic -</h3>
+          <h3>-Basic-</h3>
           <span className='bar'>
             <p className='btc'> 1 BTC</p>
-            <p>- 3 days -</p>
-            <p>- Views -</p>
-            <p>- Featured -</p>
-            <p>- Private Quaters -</p>
+            <p>-3 days-</p>
+            <p>-Views-</p>
+            <p>-Featured-</p>
+            <p>-Private Quaters-</p>
           </span>
           <Link to='/contact' className='btn'>Book</Link>
         </div>
 
         <div className="card">
-          <h3>- Suite -</h3>
+          <h3>-Suite-</h3>
           <span className='bar'>
             <p className='btc'> 1 BTC</p>
-            <p>- 3 days -</p>
-            <p>- Views -</p>
-            <p>- Featured -</p>
-            <p>- Private Quaters -</p>
+            <p>-3 days-</p>
+            <p>-Views-</p>
+            <p>-Featured-</p>
+            <p>-Private Quaters-</p>
           </span>
           <Link to='/contact' className='btn'>Book</Link>
         </div>
 
         <div className="card">
-          <h3>- Executive -</h3>
+          <h3>-Executive-</h3>
           <span className='bar'>
             <p className='btc'> 1 BTC</p>
-            <p>- 3 days -</p>
-            <p>- Views -</p>
-            <p>- Featured -</p>
-            <p>- Private Quaters -</p>
+            <p>-3 days-</p>
+            <p>-Views-</p>
+            <p>-Featured-</p>
+            <p>-Private Quaters-</p>
           </span>
           <Link to='/contact' className='btn'>Book</Link>
         </div>

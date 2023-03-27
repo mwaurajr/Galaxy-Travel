326fdad46e14f169635ffac1d3ab1d9ac84e4121
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-03-27 06:33:11 +0300

    Added training routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Training from "./components/Training";
 import Home from "./routes/Home";
 import Pricing from "./routes/Pricing";
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/pricing" element={<Pricing />} />
+        <Route path="/training" element={<Training />} />
       </Routes>
     </>
   );
